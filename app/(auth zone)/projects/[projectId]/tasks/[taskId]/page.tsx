e283afc8463b2943df6ad7065d60758678d353c6
Mikhail Foye <wasdbit@gmail.com>
2023-12-10 19:34:33 +0500

    TP-5 реализована страница с предложениями

--- app/(auth zone)/projects/[projectId]/tasks/[taskId]/page.tsx
+++ app/(auth zone)/projects/[projectId]/tasks/[taskId]/page.tsx
@@ -7,13 +7,15 @@ import { getCardById } from '@/api/Api'
 import { CardResponse } from '@/api/dataСontracts'
 import { useEffect, useState } from 'react'
 import { useParams } from 'next/navigation'
+import Container from '@mui/material/Container'
+import * as React from 'react'
+import { Grid } from '@mui/material'
 
 const card = "w-full inset-x-0 aspect-video px-4 rounded-lg ease-out duration-300 hover:shadow"
 const grayCard = `${card} bg-neutral-300 hover:bg-gray-400`
 const skeletonCard = `${card} bg-neutral-300 animate-pulse`
 
 export default function Page() {
-    const params = useParams()
     const { projectId, taskId } = useParams();
     const [status, setStatus] = useState('loading')
     const [task, setTask] = useState({} as CardResponse)
@@ -37,43 +39,45 @@ export default function Page() {
 
     return (
         <main>
-            <div className="pt-4 mr-5 grid grid-cols-4 gap-5">
-                <div className="col-start-2 col-span-2 w-full h-full px-10 py-4 rounded-lg bg-gray-300 space-y-3">
-                    <div className="flex justify-center font-bold space-x-3">
-                        <h1>{task.title}</h1>
-                        <div className="flex items-center">
-                            {/* <select name="status" id="status" className="flex justify-center rounded-md bg-orange-300 text-center text-sm w-fit">
-                                <option value="new">НОВЫЙ</option>
-                                <option value="inprogress">В РАБОТЕ</option>
-                                <option value="accepted">ПРИНЯТЫЙ</option>
-                                <option value="declined">ОТКЛОНЁННЫЙ</option>
-                            </select> */}
+            <Container fixed>
+                <Grid
+                    container
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    className="min-h-screen"
+                >
+                    <div className="w-3/5 py-4 rounded-lg bg-neutral-200 space-y-2 inset-x-0 mb-12">
+                        <div className="flex justify-center space-x-3 mb-4">
+                            <span className="font-bold text-2xl flex">{task.title}</span>
+                            <span className="font-bold flex items-center px-2 rounded-md bg-orange-300 text-sm w-fit">{task.status}</span>
                         </div>
+                        <div className="flex m-16 items-center space-x-3">
+                            <p>
+                                <AccountCircleIcon sx={{ fontSize: 50 }} />
+                            </p>
+                            <span className="text-neutral-600">{task.authorId}</span>
+                        </div>
+                        <div className="flex justify-center items-end text-4xl">
+                            <h1>{task.upVote}&nbsp;
+                                <button>
+                                    <ThumbUpIcon sx={{ fontSize: 36 }} />
+                                </button>
+                                &nbsp;| {task.downVote}&nbsp;
+                                <button>
+                                    <ThumbDownIcon sx={{ fontSize: 36 }} />
+                                </button>
+                            </h1>
+                        </div>
+                        <div className="px-8 ">
+                            <span className="text-neutral-600">Описание</span>
+                            <div className="flex">
+                                <span>{task.summary}</span>
+                            </div>
+                        </div>                       
                     </div>
-                    <div className="flex space-x-3">
-                        <p>
-                            <AccountCircleIcon sx={{ fontSize: 50 }} />
-                        </p>
-                    </div>
-                    <div className="flex justify-center items-end text-4xl">
-                        <h1>2&nbsp;
-                            <button>
-                                <ThumbUpIcon sx={{ fontSize: 36 }} />
-                            </button>
-                            &nbsp;| 0&nbsp;
-                            <button>
-                                <ThumbDownIcon sx={{ fontSize: 36 }} />
-                            </button>
-                        </h1>
-                    </div>
-                    <div className="flex space-x-3">
-                        <label>Описание</label>
-                    </div>
-                    <div className="flex justify-center">
-                        <input type="text" name="description" className="w-full bg-gray-200 flex-wrap" />
-                    </div>
-                </div>
-            </div>
+                </Grid>
+            </Container>             
         </main>
     )
 }
